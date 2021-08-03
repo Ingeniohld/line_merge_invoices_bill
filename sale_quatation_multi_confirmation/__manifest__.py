507f24ff8ec0c0c57ab417f194ee7481aaa1b5bc
Ingeniohld <78501746+Ingeniohld@users.noreply.github.com>
2021-08-03 21:47:25 +0300

    Add files via upload

---
+++ sale_quatation_multi_confirmation/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Quotation Multi Confirmation',
+    'version': '1.0',
+    'summary': 'Sale Quotation Multi Confirmation module is used to confirm multiple sale orders at the same time. Users can confirm multiple orders in the tree view.',
+    'description': """
+This module is used to confirm multiple sale orders at the same time.
+    """,
+
+    'category': 'Sales/Sales',
+    'license': "OPL-1",
+    'author': 'Ingenio',
+    'depends': ['sale_management'],
+    'data': ['data/data.xml'],
+    'images': ['static/description/banner.jpg'],
+    'price': 8,
+    'currency': 'EUR',
+    'sequence': 1,
+    'installable': True,
+}
